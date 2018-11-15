3063a7f8a3d42d4fe4ce4ccf114982f6228ea407
Bashar <babduljawad@inauth.com>
2018-11-15 15:46:32 -0700

    log4j2

--- lib/log4jruby/support/bootstrap.rb
+++ lib/log4jruby/support/bootstrap.rb
@@ -1,6 +1,6 @@
 require 'thread'
 
-Java::org.apache.logging.log4j.Logger.class_eval do
+Java::org.apache.logging.log4j.core.Logger.class_eval do
   attr_accessor :ruby_logger
   @ruby_logger_lock = Mutex.new
 
@@ -11,13 +11,15 @@ Java::org.apache.logging.log4j.Logger.class_eval do
   end
 
   def ruby_logger
-    Log4jruby::Logger.new(self)
+    self.class.ruby_logger_lock.synchronize do
+      @ruby_logger ||= Log4jruby::Logger.new(self)
+    end
   end
 end
 
 # https://github.com/lenny/log4jruby/issues/14
 # https://github.com/jruby/jruby/wiki/Persistence
-if Java::org.apache.logging.log4j.Logger.respond_to?(:__persistent__)
-  Java::org.apache.logging.log4j.Logger.__persistent__ = true
+if Java::org.apache.logging.log4j.core.Logger.respond_to?(:__persistent__)
+  Java::org.apache.logging.log4j.core.Logger.__persistent__ = true
 end
 
